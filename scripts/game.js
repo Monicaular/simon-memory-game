8f49523b1bc352d4cce4c674def11fd566294309
Monicaular <moniq.iancu@gmail.com>
2024-02-21 17:36:13 +0000

    Add more functions and test them

--- scripts/game.js
+++ scripts/game.js
@@ -5,4 +5,29 @@ let game = {
     choices: ["button1", "button2", "button3", "button4"],
 }
 
-module.exports = {game};
+function newGame() {
+    game.score = 0; //set the game score to reset to zero
+    game.currentGame = [];
+    game.playerMoves = [];
+    showScore();
+    addTurn();
+}
+
+function showScore() {
+   document.getElementById("score").innerText = game.score;
+}
+
+function addTurn() {
+    game.playerMoves = [];
+    game.currentGame.push(game.choices[(Math.floor(Math.random() *4))]);
+/*
+We're going to push onto  the computer game sequence,  
+we're going to go to our game.choices  key, which of course contains our four values,
+the IDs of our buttons. And then  we're going to use the math.random library  
+to generate a random number between zero and  three. We're going to use that as the index  
+of our choices array and then the resulting  choice is pushed onto the current game array.
+*/
+    //showTurns();
+}
+
+module.exports = {game, newGame, showScore, addTurn};
--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -3,7 +3,7 @@
  * @jest-environment jsdom
  */
 
-const { game } = require("../game");
+const { game, newGame, showScore, addTurn } = require("../game");
 beforeAll(() => {
     let fs = require("fs");
     let fileContents = fs.readFileSync("index.html", "utf-8");
@@ -27,5 +27,32 @@ describe("game object contains correct keys", () => {
     });
     test("choices contains the correct Ids", () => {
         expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+    });
+});
+
+//see if the new game function resets the score
+describe ("new game works correctly", () => {
+    beforeAll(() => {
+        game.score = 42;
+        game.playerMoves = [1, 2, 3];
+        game.currentGame = [1, 2, 3];
+        document.getElementById("score").innerText = "42";//set the score on the DOM to 42 to see if it will reset to 0 by newGame
+        newGame();
+    });
+    test("should set the game score to zero", () => {
+        expect(game.score).toEqual(0);
+    });
+    test("should clear the playerMoves array", () => {
+        expect(game.playerMoves.length).toBe(0);
+    });
+    // test("should clear the computer sequence array", () => {
+    //     expect(game.currentGame.length).toBe(0);
+    // });
+    //replaced the above with the below
+    test("should be one element in the computer's array", () => {
+        expect(game.currentGame.length).toBe(1);
+    });
+    test("should display 0 for the element with the id of score", () => {
+        expect(document.getElementById("score").innerText).toEqual(0);
     });
 });
